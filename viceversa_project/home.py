7bb15cf8d9ee50b280d1e3a9d038b5771daf7d9e
LostMasster <lostmasster2@gmail.com>
2024-10-30 09:50:13 +0100

    Add reverse

---
+++ viceversa_project/home.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# komandy
+# django-admin startproject myproject - sozdajot proekt
+# python manage.py migrate - migracija proekta
+# python manage.py runserver - zapusk proekta
+
+
+def startovaja(request):
+    return HttpResponse('Poluchilos')
+
+def home(request):
+    return render(request, 'home.html', {'greeting': 'Hallo!'})
+
+def some_page(request):
+    usertext = request.GET['user_text']
+    reversed_text = usertext[::-1]
+    return render(request, 'some_page.html',
+                  {'user_text': usertext, 'reversedtext': reversed_text})
